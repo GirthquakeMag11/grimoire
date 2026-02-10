6369fb32ba68328faf5b69fd79fc0ce7bcf01f00
Girthquake <captainpatchwork@gmail.com>
2026-02-10 00:52:30 -0900

    Update strings.py

--- src/grim/strings.py
+++ src/grim/strings.py
@@ -6,7 +6,6 @@ LOWERCASE_ALPHA = "abcdefghijklmnopqrstuvwxyz"
 UPPERCASE_ALPHA = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 COMBINED_ALPHA = UPPERCASE_ALPHA + LOWERCASE_ALPHA
 
-
 def surroundedwith(string: str, start: str, end: Optional[str] = None) -> bool:
     """Return a boolean representing whether 'string' argument starts with
     'start' substring and ends with 'end' substring.
