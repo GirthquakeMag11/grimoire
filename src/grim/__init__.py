69bb3b09ca2b91d43b48039b159f8e836ad1e152
Girthquake <captainpatchwork@gmail.com>
2026-02-09 19:34:13 -0900

    Update __init__.py

--- src/grim/__init__.py
+++ src/grim/__init__.py
@@ -1,9 +1,14 @@
 from . import attributes
 from . import concurrency
 from . import decorations
+from . import utilities
 
 __all__ = [
+
+    # Modules
     'attributes',
     'concurrency',
     'decorations',
+    'utilities',
+
     ]
