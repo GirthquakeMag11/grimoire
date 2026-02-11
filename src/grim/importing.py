12c8e88cacce06a226355a3a12c72c8aa565570c
Girthquake <captainpatchwork@gmail.com>
2026-02-11 04:08:05 -0900

    Update importing.py

--- src/grim/importing.py
+++ src/grim/importing.py
@@ -7,29 +7,26 @@ from typing import Optional, TypeAlias, Union
 
 Module: TypeAlias = types.ModuleType
 
-def import_module_from_path(module_path: Union[str, Path], module_name: Optional[str] = None) -> Module:
+
+def import_module_from_path(
+    module_path: Union[str, Path], module_name: Optional[str] = None
+) -> Module:
     module_path = Path(module_path).resolve()
     if not module_name:
         module_name = str(module_path.name)
     if module_path.is_file():
-        spec = importlib.util.spec_from_file_location(
-            module_name, str(module_path)
-        )
+        spec = importlib.util.spec_from_file_location(module_name, str(module_path))
     elif module_path.is_dir():
-        init_file = module_path / '__init__.py'
+        init_file = module_path / "__init__.py"
         if not init_file.is_file():
             raise FileNotFoundError(
                 f"Directory '{module_path}' is not a valid package (missing __init__.py)"
             )
         spec = importlib.util.spec_from_file_location(
-            module_name,
-            str(init_file),
-            submodule_search_locations=[str(module_path)]
-            )
-    else:
-        raise FileNotFoundError(
-            f"No such file or directory: {module_path!s}"
+            module_name, str(init_file), submodule_search_locations=[str(module_path)]
         )
+    else:
+        raise FileNotFoundError(f"No such file or directory: {module_path!s}")
 
     if spec is None or spec.loader is None:
         raise ImportError(
