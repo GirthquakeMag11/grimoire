5107c98288f006c2a015f0d1a0ec835fc707df31
Girthquake <captainpatchwork@gmail.com>
2026-02-11 04:07:58 -0900

    Update files.py

--- src/grim/files.py
+++ src/grim/files.py
@@ -7,9 +7,11 @@ def ensure_path(path: str | Path) -> Path:
     """Coerce 'path' into Path instance and resolve it."""
     return Path(path).resolve(strict=False)
 
+
 def ensure_path_string(path: str | Path) -> str:
     """Resolve 'path' then ensure it is a string."""
     return str(ensure_path(path))
+
 
 def path_exists(path: str | Path) -> bool:
     try:
@@ -18,11 +20,13 @@ def path_exists(path: str | Path) -> bool:
     except FileNotFoundError:
         return False
 
+
 def path_is_file(path: str | Path) -> bool:
     try:
         return Path(path).resolve(strict=True).is_file()
     except FileNotFoundError:
         return False
+
 
 def path_is_dir(path: str | Path) -> bool:
     try:
