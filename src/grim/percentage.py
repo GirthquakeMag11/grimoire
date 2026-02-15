4250f0e031bc1982d83460dfcf90cd3f6b0b4948
Girthquake <captainpatchwork@gmail.com>
2026-02-15 02:10:44 -0900

    Update percentage.py

--- src/grim/percentage.py
+++ src/grim/percentage.py
@@ -2,37 +2,51 @@ from __future__ import annotations
 
 from typing import Union, NamedTuple
 
+
 def percentage_from_whole_and_part(part: Union[int, float], whole: Union[int, float]):
-    return ((part / whole) * 100)
+    return (part / whole) * 100
 
-def part_from_whole_and_percentage(percentage: Union[int, float], whole: Union[int, float]):
-    return (percentage * whole)
 
-def whole_from_part_and_percentage(part: Union[int, float], percentage: Union[int, float]):
-    return (part / percentage)
+def part_from_whole_and_percentage(
+    percentage: Union[int, float], whole: Union[int, float]
+):
+    return percentage * whole
+
+
+def whole_from_part_and_percentage(
+    part: Union[int, float], percentage: Union[int, float]
+):
+    return part / percentage
+
 
 def if_whole_is_none(whole, part, percentage):
     if whole is None and part is not None and percentage is not None:
         return (whole_from_part_and_percentage(part, percentage), part, percentage)
     return (whole, part, percentage)
 
+
 def if_part_is_none(whole, part, percentage):
     if part is None and whole is not None and percentage is not None:
         return (whole, part_from_whole_and_percentage(percentage, whole), percentage)
     return (whole, part, percentage)
+
 
 def if_percentage_is_none(whole, part, percentage):
     if percentage is None and part is not None and whole is not None:
         return (whole, part, percentage_from_whole_and_part(whole, part))
     return (whole, part, percentage)
 
+
 def ensure_percentage_params(whole, part, percentage):
-    return if_whole_is_none(if_part_is_none(if_percentage_is_none(whole, part, percentage)))
+    return if_whole_is_none(
+        if_part_is_none(if_percentage_is_none(whole, part, percentage))
+    )
+
 
 def percentage_params(*args, **kwargs):
-    whole = kwargs.get('whole', None)
-    part = kwargs.get('part', None)
-    percentage = kwargs.get('percentage', None)
+    whole = kwargs.get("whole", None)
+    part = kwargs.get("part", None)
+    percentage = kwargs.get("percentage", None)
     if whole is None and len(args) >= 1:
         whole = args[0]
     if part is None and len(args) >= 2:
@@ -48,26 +62,32 @@ def percentage_params(*args, **kwargs):
     else:
         return ("_insufficient_values_", whole, part, percentage)
 
+
 def compare_calculation_part_percentage(whole, part, percentage):
-    return (whole == whole_from_part_and_percentage(part, percentage))
+    return whole == whole_from_part_and_percentage(part, percentage)
+
 
 def compare_calculation_percentage_whole(whole, part, percentage):
-    return (part == part_from_whole_and_percentage(whole, percentage))
+    return part == part_from_whole_and_percentage(whole, percentage)
+
 
 def compare_calculation_whole_part(whole, part, percentage):
-    return (percentage == percentage_from_whole_and_part(whole, part))
+    return percentage == percentage_from_whole_and_part(whole, part)
+
 
 def calculation_comparison(whole, part, percentage):
     return (
         compare_calculation_part_percentage(whole, part, percentage),
         compare_calculation_percentage_whole(whole, part, percentage),
         compare_calculation_whole_part(whole, part, percentage),
-        )
+    )
+
 
 class Percentage(NamedTuple):
     whole: Union[int, float]
     part: Union[int, float]
     percentage: Union[int, float]
+
 
 def percent(*args, **kwargs):
     call_type, whole, part, percentage = percentage_params(*args, **kwargs)
