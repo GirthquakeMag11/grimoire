660103c14b57360c303361e0bcfa8894799d4cb6
Girthquake <captainpatchwork@gmail.com>
2026-02-15 02:19:28 -0900

    Update markov.py

--- src/grim/markov.py
+++ src/grim/markov.py
@@ -4,14 +4,14 @@ from __future__ import annotations
 
 import collections.abc
 from dataclasses import InitVar, dataclass, field
-from typing import Dict, Hashable, Iterable, Sequence, Tuple, TypeVar
+from typing import Hashable, Iterable, Sequence, TypeVar
 
 T = TypeVar("T", bound=Hashable)
 
 
-def m_transitions(seq: Sequence[T]) -> Dict[T, Dict[T, int]]:
+def m_transitions(seq: Sequence[T]) -> dict[T, dict[T, int]]:
     """Count transitions between consecutive items."""
-    transitions: Dict[T, Dict[T, int]] = {}
+    transitions: dict[T, dict[T, int]] = {}
 
     for i in range(len(seq) - 1):
         head, tail = seq[i], seq[i + 1]
@@ -21,9 +21,9 @@ def m_transitions(seq: Sequence[T]) -> Dict[T, Dict[T, int]]:
     return transitions
 
 
-def m_probabilities(transitions: Dict[T, Dict[T, int]]) -> Dict[T, Dict[T, float]]:
+def m_probabilities(transitions: dict[T, dict[T, int]]) -> dict[T, dict[T, float]]:
     """Convert transition counts to probabilities."""
-    probabilities: Dict[T, Dict[T, float]] = {}
+    probabilities: dict[T, dict[T, float]] = {}
 
     for current_head_item, tail_transition_counts in transitions.items():
         total_transitions = sum(tail_transition_counts.values())
@@ -45,16 +45,19 @@ class Markov[T: Hashable]:
 
     data: InitVar[Iterable[T]]
 
-    values: Tuple[T, ...] = field(init=False)
-    transitions: Dict[T, Dict[T, int]] = field(init=False)
-    probabilities: Dict[T, Dict[T, float]] = field(init=False)
+    values: tuple[T, ...] = field(init=False)
+    transitions: dict[T, dict[T, int]] = field(init=False)
+    probabilities: dict[T, dict[T, float]] = field(init=False)
 
     def __post_init__(self, data: Iterable[T]) -> None:
         object.__setattr__(self, "values", tuple(data))
         object.__setattr__(self, "transitions", m_transitions(self.values))
         object.__setattr__(self, "probabilities", m_probabilities(self.transitions))
 
-    def __add__(self, other):
+    def __getitem__(self, key: T) -> dict[T, int]:
+        return self.probabilities.get(key, {})
+
+    def __add__(self, other) -> Markov | NotImplemented:
         """Concatenate with another markov instance or iterable."""
         if isinstance(other, type(self)):
             other_values = other.values
