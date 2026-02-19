caf34d18ffe4b21d8ecf47b8e3d1e9df213a0bb3
Girthquake <captainpatchwork@gmail.com>
2026-02-19 14:05:09 -0900

    Create automata.py

---
+++ src/grim/automata.py
@@ -0,0 +1,19 @@
+from __future__ import annotations
+
+from abc import ABC, abstractmethod
+
+
+class Component:
+    ...
+
+class Command:
+    ...
+
+class Event:
+    ...
+
+class Task:
+    ...
+
+class Automata:
+    ...
