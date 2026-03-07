09b964e6888288a0d8314b9ec1c1024e8730a210
Girthquake <captainpatchwork@gmail.com>
2026-03-07 03:31:42 -0900

    Create sequences.py

---
+++ src/grim/sequences.py
@@ -0,0 +1,85 @@
+from __future__ import annotations
+
+from collections.abc import (
+    Iterable,
+    Iterator,
+)
+from collections.abc import (
+    MutableSequence as AbstractMutableSequence,
+)
+from typing import Self, cast, overload
+
+
+class DistinctSequence[T](AbstractMutableSequence[T]):
+    def __init__(self, data: Iterable[T] = ()) -> None:
+        self._data: list[T] = list(data)
+
+    @overload
+    def __getitem__(self, index: int) -> T: ...
+    @overload
+    def __getitem__(self, index: slice) -> list[T]: ...
+    def __getitem__(self, index: int | slice) -> T | list[T]:
+        return self._data[index]
+
+    @overload
+    def __setitem__(self, index: int, value: T) -> None: ...
+    @overload
+    def __setitem__(self, index: slice, value: Iterable[T]) -> None: ...
+    def __setitem__(self, index: int | slice, value: T | Iterable[T]) -> None:
+        if isinstance(index, slice):
+            if not isinstance(value, Iterable):
+                raise TypeError(f"can only assign an iterable to a slice, not {type(value)}")
+            self._data[index] = list(value)
+        else:
+            self._data[index] = cast(T, value)
+
+    def __delitem__(self, index: int | slice) -> None:
+        del self._data[index]
+
+    def __len__(self) -> int:
+        return len(self._data)
+
+    def insert(self, index: int, value: T) -> None:
+        self._data.insert(index, value)
+
+    def __contains__(self, value: object) -> bool:
+        return value in self._data
+
+    def __iter__(self) -> Iterator[T]:
+        return iter(self._data)
+
+    def __reversed__(self) -> Iterator[T]:
+        return reversed(self._data)
+
+    def __iadd__(self, values: Iterable[T]) -> Self:
+        self._data.extend(values)
+        return self
+
+    def append(self, value: T) -> None:
+        self._data.append(value)
+
+    def prepend(self, value: T) -> None:
+        self._data.insert(0, value)
+
+    def clear(self) -> None:
+        self._data.clear()
+
+    def reverse(self) -> None:
+        self._data.reverse()
+
+    def extend(self, values: Iterable[T]) -> None:
+        self._data.extend(values)
+
+    def pop(self, index: int = -1) -> T:
+        return self._data.pop(index)
+
+    def remove(self, value: T) -> None:
+        self._data.remove(value)
+
+    def index(
+        self, value: T, start: int = 0, stop: int = 9223372036854775807
+    ) -> int:  # sys.maxsize
+        return self._data.index(value, start, stop)
+
+    def count(self, value: T) -> int:
+        return self._data.count(value)
