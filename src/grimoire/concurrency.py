e17a4e46cf031f450c20c0877d6831b399211bb8
Girthquake <captainpatchwork@gmail.com>
2026-04-12 17:15:29 -0800

    Create concurrency.py

---
+++ src/grimoire/concurrency.py
@@ -0,0 +1,14 @@
+from __future__ import annotations
+
+from collections.abc import AsyncIterator, Iterator
+from typing import Any, overload
+
+async def achain(*iterables: Iterator[Any] | AsyncIterator[Any]) -> AsyncIterator[Any]:
+    for iterable in iterables:
+        if isinstance(iterable, AsyncIterator):
+            async for item in iterable:
+                yield item
+
+        elif isinstance(iterable, Iterator):
+            yield from iterable
+
