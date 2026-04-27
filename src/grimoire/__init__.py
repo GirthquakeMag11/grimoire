820797a186b88f6e6e6cd0a82b703bc6f0edfd4f
Girthquake <captainpatchwork@gmail.com>
2026-04-26 17:17:48 -0800

    Update __init__.py

--- src/grimoire/__init__.py
+++ src/grimoire/__init__.py
@@ -1,17 +1,17 @@
+from ._test import _test
 from .inspection import (
-    iter_fields,
-    iter_attributes,
     compose_signature,
     decompose_attrs,
+    iter_attributes,
+    iter_fields,
     tree_attrs,
 )
 from .record import Record
-from ._test import _test
 
 __all__ = [
-    "iter_fields",
-    "iter_attributes",
     "compose_signature",
     "decompose_attrs",
+    "iter_attributes",
+    "iter_fields",
     "tree_attrs",
 ]
