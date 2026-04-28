6cb0b97b93856be9a24befba04091bf31453cef5
Girthquake <captainpatchwork@gmail.com>
2026-04-28 00:50:43 -0800

    Update data.py

--- src/grimoire/data.py
+++ src/grimoire/data.py
@@ -1,55 +1,45 @@
 from __future__ import annotations
 
-import inspect
-import types
-from collections.abc import Callable, Iterator
-from datetime import UTC, datetime
+from dataclasses import (
+    dataclass,
+    is_dataclass,
+    field,
+    Field,
+    MISSING,
+    _MISSING_TYPE as MissingType,
+)
+from functools import (
+    cached_property,
+    partial,
+)
+from inspect import (
+    signature as inspect_signature,
+    isclass as inspect_isclass,
+    Parameter,
+    Signature,
+    _ParameterKind as ParameterKind,
+)
+from types import (
+    MappingProxyType,
+    UnionType,
+)
+from collections.abc import (
+    Callable,
+    Iterator,
+)
 from typing import (
     Any,
+    Protocol,
+    Annotated,
+    Literal,
+    runtime_checkable,
     get_type_hints,
+    get_origin,
+    get_args,
+    TypeAliasType,
+    TypedDict,
+    Union,
 )
-
-
-def utcnow() -> datetime:
-    return datetime.now(UTC)
-
-
-def compose_signature(obj: Callable[[...], ...]) -> dict[str, Any]:
-    signature: dict[str, Any] = {}
-    hints: dict[str, Any] = get_type_hints(obj)
-
-    if "return" in hints:
-        signature["return"] = hints["return"]
-
-    for idx, (keyword, param) in enumerate(inspect.signature(obj).parameters.items()):
-        parameter = {}
-
-        if param.kind is inspect.Parameter.VAR_KEYWORD:
-            parameter["unpack"] = dict
-
-        if param.kind is inspect.Parameter.VAR_POSITIONAL:
-            parameter["unpack"] = tuple
-
-        if param.kind in (inspect.Parameter.VAR_KEYWORD, inspect.Parameter.KEYWORD_ONLY):
-            parameter["position"] = None
-
-        if param.kind in (
-            inspect.Parameter.POSITIONAL_ONLY,
-            inspect.Parameter.POSITIONAL_OR_KEYWORD,
-            inspect.Parameter.VAR_POSITIONAL,
-        ):
-            parameter["position"] = idx
-
-        if param.default is not inspect.Parameter.empty:
-            parameter["default"] = param.default
-
-        if keyword in hints:
-            parameter["annotation"] = hints.get(keyword)
-
-        if parameter:
-            signature.setdefault("parameters", {})[keyword] = parameter
-
-    return signature
 
 
 def iter_fields(obj: Any) -> Iterator[str]:
@@ -112,8 +102,155 @@ def iter_attributes(obj: Any) -> Iterator[tuple[str, Any]]:
             continue
 
 
-<<<<<<< Updated upstream
-=======
-def copy_namespace(obj: Any) -> types.SimpleNamespace:
-    return types.SimpleNamespace(iter_attributes(obj))
->>>>>>> Stashed changes
+def create_parameter(name: str, kind: str, annotation: Any = MISSING, default: Any = MISSING) -> Parameter:
+    p_kind: ParameterKind
+    match kind.strip().casefold():
+        case "positional_only":
+            p_kind = ParameterKind.POSITIONAL_ONLY
+        case "positional_or_keyword":
+            p_kind = ParameterKind.POSITIONAL_OR_KEYWORD
+        case "var_positional":
+            p_kind = ParameterKind.VAR_POSITIONAL
+        case "keyword_only":
+            p_kind = ParameterKind.KEYWORD_ONLY
+        case "var_keyword":
+            p_kind = ParameterKind.VAR_KEYWORD
+        case _:
+            raise ValueError(kind)
+
+    factory = partial(Parameter, name, p_kind)
+    kwargs = {
+        "annotation": annotation if annotation is not MISSING else Parameter.empty,
+        "default": default if default is not MISSING else Parameter.empty,
+    }
+    return Parameter(name, p_kind, **kwargs)
+
+
+def create_signature(*parameters: Parameter, return_annotation: Any = MISSING) -> Signature:
+    return Signature(
+        list(parameters),
+        return_annotation=return_annotation if return_annotation is not MISSING else Parameter.empty,
+    )
+
+@dataclass
+class TypeNode:
+    target: Any
+
+    @cached_property
+    def name(self) -> str | None:
+        if self.is_class or self.is_type_alias:
+            return self.target.__name__
+        return None
+
+    @cached_property
+    def is_generic(self) -> bool:
+        return self.origin is not None and not self.is_literal and not self.is_union
+
+    @cached_property
+    def type_params(self) -> tuple[Any, ...]:
+        if self.is_type_alias:
+            return self.target.__type_params__
+        return ()
+
+    @cached_property
+    def is_type_alias(self) -> bool:
+        return isinstance(self.target, TypeAliasType)
+
+    @cached_property
+    def is_class(self) -> bool:
+        return inspect_isclass(self.origin)
+
+    @cached_property
+    def is_dataclass(self) -> bool:
+        return isinstance(self.target, type) and is_dataclass(self.target)
+
+    @cached_property
+    def is_optional(self) -> bool:
+        return self.is_union and type(None) in self.union_values
+
+    @cached_property
+    def option_values(self) -> tuple[Any, ...]:
+        if self.is_optional:
+            return tuple(arg for arg in self.union_values if arg is not type(None))
+        return ()
+
+    @cached_property
+    def is_union(self) -> bool:
+        return self.origin is Union or self.origin is UnionType
+
+    @cached_property
+    def union_values(self) -> tuple[Any, ...]:
+        if self.is_union:
+            return get_args(self.inner_type)
+        return ()
+
+    @cached_property
+    def inner_type(self) -> Any:
+        if self.is_annotated:
+            return get_args(self.target)[0]
+        elif self.is_type_alias:
+            return self.target.__value__
+        return self.target
+
+    @cached_property
+    def is_annotated(self) -> bool:
+        return get_origin(self.target) is Annotated
+
+    @cached_property
+    def annotated_extras(self) -> tuple[Any, ...]:
+        if self.is_annotated:
+            return get_args(self._raw)[1:]
+        return ()
+
+    @cached_property
+    def is_literal(self) -> bool:
+        return self.origin is Literal
+
+    @cached_property
+    def literal_values(self) -> tuple[Any, ...]:
+        if self.is_literal:
+            return get_args(self.inner_type)
+        return ()
+
+    @cached_property
+    def origin(self) -> Any | None:
+        return get_origin(self.inner_type)
+
+    @cached_property
+    def args(self) -> tuple[TypeNode, ...]:
+        if self.is_literal:
+            return ()
+        return tuple(TypeNode(a) for a in get_args(self.inner_type))
+
+@dataclass
+class FieldNode:
+    target: Any
+
+    @cached_property
+    def name(self) -> str | None:
+        if self.is_param or self.is_field:
+            return self.target.name
+        return None
+
+    @cached_property
+    def is_param(self) -> bool:
+        return isinstance(self.target, Parameter)
+
+    @cached_property
+    def is_field(self) -> bool:
+        return isinstance(self.target, Field)
+
+
+@dataclass
+class SignatureNode:
+    target: Any
+
+    @cached_property
+    def parameters(self) -> tuple[FieldNode, ...]:
+        return tuple(FieldNode(value) for value in inspect_signature(self.target).parameters)
+
+    @cached_property
+    def return_annotation(self) -> TypeNode | MISSING:
+        if (anno := get_type_hints(self.target, include_extras=True).get("return", None)) is not None:
+            return TypeNode(anno)
+        return MISSING
